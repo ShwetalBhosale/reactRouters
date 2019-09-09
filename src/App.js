913f4ffeca29696e9dee28371c3c1e776de46e45
ShwetalBhosale <53152530+ShwetalBhosale@users.noreply.github.com>
2019-09-09 16:25:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header/>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </div>
+    </Router>
+  );
+}
+
+
+function Header() {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/topics">Topics</Link>
+      </li>
+    </ul>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Topic({ match }) {
+  return <h3>Requested Param: {match.params.id}</h3>;
+}
+
+function Topics({ match }) {
+    /*A match object contains information about how a <Route path> matched the URL
+    params - (object) Key/value pairs parsed from the URL corresponding to the dynamic segments of the path
+    isExact - (boolean) true if the entire URL was matched (no trailing characters)
+    path - (string) The path pattern used to match. Useful for building nested <Route>s
+    url - (string) The matched portion of the URL. Useful for building nested <Link>s    */
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${match.url}/componentssss`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.path}/:id`} component={Topic} />
+      <Route
+        exact
+        path={match.path}
+        render={() => <h3>Please select a topic.</h3>}
+      />
+    </div>
+  );
+}
+
+
+export default App;
